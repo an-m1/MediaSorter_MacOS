1f4bf15f88c1d47db0d8d2746eb2888b19dd40da
Ankit Modhera <ankitmodhera24@gmail.com>
2025-12-12 21:49:16 -0500

    Initial Commit

---
+++ MediaSorter/MediaSorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaSorterApp.swift
+//  MediaSorter
+//
+//  Created by Ankit Modhera on 2025-12-12.
+//
+
+import SwiftUI
+
+@main
+struct MediaSorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaSorterTests/MediaSorterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaSorterTests.swift
+//  MediaSorterTests
+//
+//  Created by Ankit Modhera on 2025-12-12.
+//
+
+import Testing
+@testable import MediaSorter
+
+struct MediaSorterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
